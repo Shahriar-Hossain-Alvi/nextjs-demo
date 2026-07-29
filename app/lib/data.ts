b6050e4521378f1cfb21570ba29278d3128bd80f
Shahriar-Hossain-Alvi <shahriarhossainalvi@gmail.com>
2026-07-29 21:07:34 +0600

    added skeleton in dashboard page'

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -32,7 +32,6 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    console.log('Fetching latest invoices...');
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -44,7 +43,6 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
-    console.log('Latest invoices fetch completed.');
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
